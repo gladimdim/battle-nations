837a9ec6d2a3ba1cf7d11dd8e4464257d9c8df5b
Dmytro Gladkyi <gladimdim@gmail.com>
2013-02-24 19:56:14 +0200

    Added json response when game is created

--- src/battle_nations/controllers/db_bridge.clj
+++ src/battle_nations/controllers/db_bridge.clj
@@ -9,9 +9,9 @@
 (mg/set-db! (monger.core/get-db "test"))
 (insert "document" {:a 10})
 
-(defn put-player-in-queue [player-id]
+(defn put-player-in-queue [player-id army]
   "Puts player by his id into game's queue"
-  (insert "players_queue" {:player_id player-id}))
+  (insert "players_queue" {:player_id player-id :army army}))
 
 (defn get-player-from-queue []
   "Returns a player object which was matched from players_queue"
@@ -20,44 +20,39 @@
 (defn remove-player-from-queue [player-id]
   "Removes player by his id from players_queue table."
    (monger.collection/remove "players_queue" {:player_id player-id}))
-
      
-     
+(def armies {"ukraine" (battle-nations.data.ukraine/init-army) "poland" (battle-nations.data.poland/init-army)})
 
-(defn create-new-game [player-id-left player-id-right]
+(defn create-new-game [player-id-left player-id-right army-left army-right]
   "Creates new game with two player ids"
+  (let [game-id (clojure.string/join "-" [player-id-left player-id-right (gensym)])]
 	(monger.collection/insert "current_games"
-                            {:game_id (clojure.string/join "-" [player-id-left player-id-right (gensym)]),
+                            {:game_id game-id,
                              :player_left player-id-left,
                              :player_right player-id-right,
-                             :left_army {:bank {:infantry 3,
-                                                :light_cavalry 3,
-                                                :veteran 2,
-                                                :super_unit 1
-                                                },
+                             :left_army_nation army-left
+                             :left_army {:bank (armies army-left),
                                          :field {}
                                          },
-                             :right_army {:bank {:infantry 3,
-                                                :light_cavalry 3,
-                                                :veteran 2,
-                                                :super_unit 1
-                                                },
+                             :right_army_nation army-right
+                             :right_army {:bank (armies army-right)
                                          :field {}
                                          },
                              :left_army_turn true
                                           
-                            }))
+                            })
+        game-id))
 
-(defn start-new-game [player-id] 
+(defn start-new-game [player-id army] 
   "Is called when user wants to start new game. Checks if another player is in queue
   and if not - puts player-id into queue."
   (if-let [waiting-player (get-player-from-queue)]
     (when-not (= player-id (waiting-player :player_id))
-    (do (create-new-game player-id (waiting-player :player_id))
-     (pr waiting-player)
+    (let [game-id (create-new-game player-id (waiting-player :player_id) army (waiting-player :army))]
      (remove-player-from-queue (waiting-player :player_id))
+     game-id
       ))
-    ((put-player-in-queue player-id))))
+    ((put-player-in-queue player-id army))))
 
 
 (defn get-player-games [player-id]
--- src/battle_nations/views/welcome.clj
+++ src/battle_nations/views/welcome.clj
@@ -1,29 +1,31 @@
 (ns battle-nations.views.welcome
-  (:require [battle-nations.views.common :as common]
-            [noir.content.getting-started]
-            [noir.response]
+  (:require [clj-json.core :as json]
             [battle-nations.controllers.db_bridge])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to battle-nations"]))
-
-(defpage [:get  "/get-game"] {:keys [player-id]}
-  (if-let [response-data (battle-nations.db_bridge/get-player-games player-id)]
-    (noir.response/json response-data)
-    (noir.response/json {:error "No current games found."})))
-
-(defpage [:post "/current_game"] {:keys [login]}
-  (if-let [login_id login]
-    (noir.response/json {:login_ login_id})
-  	(noir.response/empty)))
-
-(defpage [:post "/want-to-play"] {:keys [player-id]} 
-  "Get player-id and check if there is another player in queue. If not - put player-id into it. If yes - 
-start new game with this player"
-  (battle-nations.controllers.db_bridge/start-new-game player-id))
-   
+  (:use [noir.core :only [defpage]])
+  (:use compojure.core)
+  (:use ring.middleware.json-params)
+  (:use ring.adapter.jetty))
 
 
+(defn json-response [data & [status]]
+  {:status (or status 200)
+   :heaeers {"Content-Type" "application/json"}
+   :body (json/generate-string data)})
 
+(defroutes handler 
+  (GET "/" []
+       (json-response {"hello" "world"}))
+  (GET "/get-game" [player-id]
+  (if-let [response-data (battle-nations.controllers.db_bridge/get-player-games player-id)]
+    (json-response response-data)
+    (json-response {:error "No current games found."})))
+  (PUT "/test" [name]
+       (json-response {"hello" name}))
+  (POST "/want-to-play" [player-id army]
+        (if (and player-id army)
+          (json-response {:game_id (battle-nations.controllers.db_bridge/start-new-game player-id army)} 200)
+          (json-response {:error "Missing parameters in request"} 500))))
+
+(def app 
+  (-> handler 
+      wrap-json-params))
